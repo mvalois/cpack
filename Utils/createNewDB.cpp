df826e045fb0ff1e4d861e417e6ef8951d4f8d1d
Jorand Jean-baptiste <jorand@ecole.ensicaen.fr>
2017-12-20 11:06:47 +0100

    update creation des db

--- Utils/createNewDB.cpp
+++ Utils/createNewDB.cpp
@@ -1,29 +1,35 @@
 #include <fstream>
-#include <string>
+#include <iomanip>
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 
 
-
-void multiplication(int coeff) {
-  wifstream readfile("../../BDD/rockyou.txt");
-  wofstream outfile("../../BDD/rockyoucustom.txt");
+void multiplication(int coeff, const string & input, const string & output) {
+  wifstream readfile(input);
+  wofstream outfile(output);
   wstring line;
+  wstring add;
 
   while(readfile.good()) {
     getline(readfile, line);
     for (int i = 0; i<coeff; i++) {
-      outfile << line << endl;
+      add = line;
+      for (int j = 0; j<i; j++) {
+        add += to_wstring(j);
+      }
+      outfile << add << endl;
     }
   }
 }
 
 
 
-void division(int coeff) {
-  wifstream readfile("../../BDD/rockyou.txt");
-  wofstream outfile("../../BDD/rockyoucustom.txt");
+void division(int coeff, const string &input, const string & output) {
+  wifstream readfile(input);
+  wofstream outfile(output);
   wstring line;
   int nbline = 0;
   
@@ -40,6 +46,13 @@ void division(int coeff) {
 int main(int argc,char* argv[]) {
   locale::global(locale(""));
   
+  if(argc < 3){
+    wcerr << "\nNeed two arguments\n\t- name of the input database\n\t- name of the output database\n" << endl;
+    return -1;
+  }
+
+  string input = argv[1];
+  string output = argv[2];
   float val;
   
   wcout << "Coefficient (>1 pour multiplication  /  <1 pour division) : " << endl;
@@ -47,9 +60,9 @@ int main(int argc,char* argv[]) {
 
   
   if (val >= 1) {
-    multiplication((int) val);
+    multiplication((int) val, input, output);
   } else {
-    division((int) 1/val);
+    division((int) 1/val, input, output);
   }
     
 
