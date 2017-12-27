8759496fb87bd9c848555040fffea62610466c7b
Bass Yannick <bass@ecole.ensicaen.fr>
2017-12-27 16:54:01 +0100

    refactor

--- AdaptationParallel/src/statsgen.cpp
+++ AdaptationParallel/src/statsgen.cpp
@@ -14,6 +14,7 @@
 #include <thread>
 #include <pthread.h>
 
+
 #include "statsgen.h"
 #include "utils.h"
 using namespace std;
@@ -229,3 +230,224 @@ void Statsgen::print_stats() {
         readResult(stats_advancedmasks[L"othermasks"], L"othermasks", count, total_counter, hiderare);
     }
 }
+
+
+void analyse_letter(const char & letter, char & last_simplemask, wstring & simplemask_string, wstring & advancedmask_string, Policy & policy, int & sizeAdvancedMask, int & sizeSimpleMask) {
+    sizeAdvancedMask++;
+
+    if (letter >= L'0' && letter <= L'9') {
+        policy.digit++;
+        advancedmask_string += L"?d";
+        if (last_simplemask != 'd') {
+            sizeSimpleMask++;
+            simplemask_string += L"digit";
+            last_simplemask = 'd';
+        }
+    }
+    else if(letter >= L'a' && letter <= L'z') {
+        policy.lower++;
+        advancedmask_string += L"?l";
+        if (last_simplemask != 'l') {
+            sizeSimpleMask++;
+            simplemask_string += L"lower";
+            last_simplemask = 'l';
+        }
+    }
+    else if(letter >= L'A' && letter <= L'Z') {
+        policy.upper++;
+        advancedmask_string += L"?u";
+        if (last_simplemask != 'u') {
+            sizeSimpleMask++;
+            simplemask_string += L"upper";
+            last_simplemask = 'u';
+        }
+    }
+    else {
+        policy.special++;
+        advancedmask_string += L"?s";
+
+        if (last_simplemask != 's') {
+            sizeSimpleMask++;
+            simplemask_string += L"special";
+            last_simplemask = 's';
+        }
+    }
+}
+
+void analyse_charset(wstring & charset, const Policy & policy) {
+    if (policy.digit && !policy.lower && !policy.upper && !policy.special) {
+        charset = L"numeric";
+    }
+    else if (!policy.digit && policy.lower && !policy.upper && !policy.special) {
+        charset = L"loweralpha";
+    }
+    else if (!policy.digit && !policy.lower && policy.upper && !policy.special) {
+        charset = L"upperalpha";
+    }
+    else if (!policy.digit && !policy.lower && !policy.upper && policy.special) {
+        charset = L"special";
+    }
+    else if (!policy.digit && policy.lower && policy.upper && !policy.special) {
+        charset = L"mixedalpha";
+    }
+    else if (policy.digit && policy.lower && !policy.upper && !policy.special) {
+        charset = L"loweralphanum";
+    }
+    else if (policy.digit && !policy.lower && policy.upper && !policy.special) {
+        charset = L"upperalphanum";
+    }
+    else if (!policy.digit && policy.lower && !policy.upper && policy.special) {
+        charset = L"loweralphaspecial";
+    }
+    else if (!policy.digit && !policy.lower && policy.upper && policy.special) {
+        charset = L"upperalphaspecial";
+    }
+    else if (policy.digit && !policy.lower && !policy.upper && policy.special) {
+        charset = L"specialnum";
+    }
+
+    else if (!policy.digit && policy.lower && policy.upper && policy.special) {
+        charset = L"mixedalphaspecial";
+    }
+    else if (policy.digit && !policy.lower && policy.upper && policy.special) {
+        charset = L"upperalphaspecialnum";
+    }
+    else if (policy.digit && policy.lower && !policy.upper && policy.special) {
+        charset = L"loweralphaspecialnum";
+    }
+    else if (policy.digit && policy.lower && policy.upper && !policy.special) {
+        charset = L"mixedalphanum";
+    }
+
+    else {
+        charset = L"all";
+    }
+}
+
+
+void analyze_password(const wstring & password, Container & c) {
+    c.pass_length = password.size();
+
+    char last_simplemask = 'a';
+    int sizeAdvancedMask = 0;
+    int sizeSimpleMask = 0;
+    wchar_t letter;
+
+    for (int i=0; i < c.pass_length; i++) {
+        letter = password[i];
+        analyse_letter(letter, last_simplemask, c.simplemask_string, c.advancedmask_string, c.pol, sizeAdvancedMask, sizeSimpleMask);
+    }
+
+    analyse_charset(c.characterset, c.pol);
+}
+
+
+
+void updateMinMax(minMax & minMaxValue, const Policy & pol) {
+    if (minMaxValue.mindigit == -1 || minMaxValue.mindigit > pol.digit) {
+        minMaxValue.mindigit = pol.digit;
+    }
+    if (minMaxValue.maxdigit == -1 || minMaxValue.maxdigit < pol.digit) {
+        minMaxValue.maxdigit = pol.digit;
+    }
+
+    if (minMaxValue.minlower == -1 || minMaxValue.minlower > pol.lower) {
+        minMaxValue.minlower = pol.lower;
+    }
+    if (minMaxValue.maxlower == -1 || minMaxValue.maxlower < pol.lower) {
+        minMaxValue.maxlower = pol.lower;
+    }
+
+    if (minMaxValue.minupper == -1 || minMaxValue.minupper > pol.upper) {
+        minMaxValue.minupper = pol.upper;
+    }
+    if (minMaxValue.maxupper == -1 || minMaxValue.maxupper < pol.upper) {
+        minMaxValue.maxupper = pol.upper;
+    }
+
+    if (minMaxValue.minspecial == -1 || minMaxValue.minspecial > pol.special) {
+        minMaxValue.minspecial = pol.special;
+    }
+    if (minMaxValue.maxspecial == -1 || minMaxValue.maxspecial < pol.special) {
+        minMaxValue.maxspecial = pol.special;
+    }
+}
+
+
+
+
+
+
+void * generate_stats_thread(void * threadarg) {
+    struct thread_data *my_data;
+    my_data = (struct thread_data *) threadarg;
+
+    wifstream readfile(my_data->filename);
+    wstring line;
+    int nbline = 0;
+
+    while(readfile.good()) {
+        ++nbline;
+        getline(readfile, line);
+        if (nbline < my_data->lineBegin) {
+            continue;
+        }
+        if (nbline > my_data->lineEnd) {
+            break;
+        }
+
+
+        if (line.size() == 0) {
+            //wcout << "Error empty password, line " << my_data->total_counter << endl;
+            continue;
+        }
+
+        Container c;
+
+        if (my_data->withcount) {
+            uint i = 0;
+            bool number=false;
+            for(i=0; i < line.length(); i++) {
+                if(iswdigit(line.at(i))) {
+                    number=true;
+                }
+                else if (!iswdigit(line.at(i)) && number) {
+                    break;
+                }
+            }
+            wstring password = line.substr(i+1,line.length());
+            int nbPasswords = stoi(line.substr(0,i));
+            
+            my_data->total_counter += nbPasswords;
+
+            if ( !my_data->use_regex || (my_data->use_regex && regex_match(password,my_data->current_regex)) ) {
+                my_data->total_filter += nbPasswords;
+                analyze_password(password, c);
+
+                my_data->length[ c.pass_length ] += nbPasswords;
+                my_data->charactersets[ c.characterset ] += nbPasswords;
+                my_data->simplemasks[ c.simplemask_string ] += nbPasswords;
+                my_data->advancedmasks[ c.advancedmask_string ] += nbPasswords;
+            }
+        }
+        else {
+            my_data->total_counter++;
+            if ( !my_data->use_regex || (my_data->use_regex && regex_match(line,my_data->current_regex)) ) {
+                analyze_password(line, c);
+                
+                my_data->total_filter++;
+
+                my_data->length[ c.pass_length ] += 1;
+                my_data->charactersets[ c.characterset ] += 1;
+                my_data->simplemasks[ c.simplemask_string ] += 1;
+                my_data->advancedmasks[ c.advancedmask_string ] += 1;
+            }
+        }
+         updateMinMax(my_data->minMaxValue, c.pol);
+    }
+
+    readfile.close();
+
+    pthread_exit(NULL);
+}
+
