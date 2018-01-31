94615af66c23a6d43ccc40a7a315d4d85adfc923
Yannick Bass <bass@ecole.ensicaen.fr>
2018-01-31 09:38:43 +0100

    license

--- AdaptationParallel/include/utils.h
+++ AdaptationParallel/include/utils.h
@@ -4,6 +4,19 @@
  *
  * @author Jean-Baptiste Jorand
  * @author Yannick Bass
+ *Copyright © 31/01/2018, Yannick BASS Jean-Baptiste JORAND
+ *
+ *Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”),
+ *to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ *The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ *The Software is provided “as is”, without warranty of any kind, express or implied, including but not limited to the warranties of merchantability,
+ *fitness for a particular purpose and noninfringement. In no event shall the authors or copyright holders X be liable for any claim, damages or other liability,
+ *whether in an action of contract, tort or otherwise, arising from, out of or in connection with the software or the use or other dealings in the Software.
+ *Except as contained in this notice, the name of Yannick BASS and Jean-Baptiste JORAND shall not be used in advertising or otherwise to promote the sale,
+ *use or other dealings in this Software without prior written authorization from Yannick BASS and Jean-Baptiste JORAND. 
  */
 
 #include <map>
@@ -41,8 +54,12 @@ multimap<double, A> flip_map(const std::unordered_map<A, double> & src) {
 
 
 /**
- * @brief print some results
- * @param 
+ * @brief print a line of statistics
+ * @param res : value of this statistic
+ * @param crac: the statistic to display
+ * @param count: the number of statistics already displayed
+ * @param total_counter:total number of password analysed
+ * @param hiderare: flag to hide statistics under 1%
  */
 template<typename Type>
 void readResult(double res, Type carac, int & count, const double & total_counter, const int & hiderare) {
