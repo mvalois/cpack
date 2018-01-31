d7b76ac4eec62cafeb3a0d9946eaf78e365456c2
Yannick Bass <bass@ecole.ensicaen.fr>
2018-01-31 10:01:31 +0100

    cleaning

--- AdaptationParallel/src/statsgen.cpp
+++ AdaptationParallel/src/statsgen.cpp
@@ -4,19 +4,10 @@
  *
  * @author Jean-Baptiste Jorand
  * @author Yannick Bass
- *Copyright © 31/01/2018, Yannick BASS Jean-Baptiste JORAND
+ * Copyright (c) 2018 Jorand Jean-baptiste Bass Yannick
+ * All rights reserved.
  *
- *Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”),
- *to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
- *and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *
- *The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- *
- *The Software is provided “as is”, without warranty of any kind, express or implied, including but not limited to the warranties of merchantability,
- *fitness for a particular purpose and noninfringement. In no event shall the authors or copyright holders X be liable for any claim, damages or other liability,
- *whether in an action of contract, tort or otherwise, arising from, out of or in connection with the software or the use or other dealings in the Software.
- *Except as contained in this notice, the name of Yannick BASS and Jean-Baptiste JORAND shall not be used in advertising or otherwise to promote the sale,
- *use or other dealings in this Software without prior written authorization from Yannick BASS and Jean-Baptiste JORAND. 
+ * Please see the attached LICENSE file for additional licensing information.
  */
 
 
